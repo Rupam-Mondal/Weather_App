b87edf56f4d5932d52ea09088340098884fef225
Rupam <rupammondal564@gmail.com>
2024-08-14 10:17:18 +0530

    Weather app first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-400'>
+        hello bro
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/helpers/Constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'fc76a5a0ff519510705cf26b927b2ec0'
+export const Openweather = 'https://api.openweathermap.org/data'
